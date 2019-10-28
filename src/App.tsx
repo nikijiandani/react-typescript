b969e2ee298565503d77cec62703d0eda8185434
Nikita Jiandani <niki.jiandani@gmail.com>
2019-10-28 15:58:31 -0400

    stateful component syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Message from './Message';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Message message='This is a simple message' />
-      </header>
-    </div>
-  );
+class App extends Component<any> {
+  componentWillMount() {
+    console.log('Almost there...');
+  }
+
+  componentDidMount() {
+    console.log('Finally...hello!');
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <Message message='This is a simple message' />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
