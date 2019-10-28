ef6f486d908cf01df13c5a8dd7fcd1bdd96b46c9
Nikita Jiandani <niki.jiandani@gmail.com>
2019-10-28 15:22:59 -0400

    tuple, enum, any, void examples

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // let firstValue: string = 'Manny';
-  // let firstValue: number = 34;
-  // let firstValue: boolean = true;
-  // let firstValue: number[] = [2, 3, 56];
-  let firstValue: Array<string> = ['hello', 'world'];
+  //tuple
+  let aTuple: [string, number] = ['Manny', 34];
+
+  //enum
+  enum Codes {
+    first = 1,
+    second = 2
+  }
+
+  //any
+  let firstName: any = 3;
+
+  //void
+  const warning = (): void => {
+    console.log('Warning');
+  };
 
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          The value {firstValue} is of {typeof firstValue} type!
+          The value {firstName} is of {typeof firstName} type!
         </p>
       </header>
     </div>
