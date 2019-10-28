86c68d75ff585e692b12ca8aa11241ab6bec6862
Nikita Jiandani <niki.jiandani@gmail.com>
2019-10-28 16:08:10 -0400

    Setup initial state with interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import Message from './Message';
 
-class App extends Component<any> {
+const initialState = {
+  name: 'Manny',
+  message: 'TypeScript is cool!!!'
+};
+
+type State = Readonly<typeof initialState>;
+
+class App extends Component<any, State> {
+  readonly state: State = initialState;
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <Message name='Manny' message='this is a simple message' />
+          <Message name={this.state.name} message={this.state.message} />
         </header>
       </div>
     );
