5b5cce41a27fb4888372e4673977dae52ef133b8
Nikita Jiandani <niki.jiandani@gmail.com>
2019-10-28 17:13:58 -0400

    Implementing HOC's in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Message from './Message';
 
-const initialState = {
-  name: 'Manny',
-  message: 'TypeScript is cool!!!'
-};
-
-type State = Readonly<typeof initialState>;
-
-class App extends Component<any, State> {
-  readonly state: State = initialState;
+class App extends Component {
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <Message name={this.state.name} message={this.state.message} />
+          <Message />
         </header>
       </div>
     );
